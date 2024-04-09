169796c4d22e0edd86b4e36ec99d785d8b3d32d1
Chingsan53 <cly53@csu.fullerton.edu>
2024-04-09 14:17:22 -0700

    added base styles

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,16 +1,133 @@
 import { Fragment } from "react";
-import "./home.styles.scss";
+import "./home.styles.css";
 
 const Home = () => {
-    return (
-        <Fragment>
-            <h1>Projects</h1>
-            <div>
-                <h2>Hello tech people</h2>
+  return (
+    <Fragment>
+      <div className="main-component">
+        <div className="profile">
+          <h2>Hello, It's Chingsan here.</h2>
+          <img
+            src="https://placehold.co/600x400.png"
+            alt="profile"
+            className="profile-pic"
+          />
+        </div>
+        <div className="works">
+          <h2>Work Experiences</h2>
+          <div className="works-container">
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 1</h3>
             </div>
-        </Fragment>
-        
-    )
-}
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 2</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 3</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 4</h3>
+            </div>
+          </div>
+        </div>
+        <div className="education">
+          <h2>Educations</h2>
+          <div className="works-container">
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 1</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 2</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 3</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 4</h3>
+            </div>
+          </div>
+        </div>
+        <div className="projects">
+          <h2>Projects</h2>
+          <div className="works-container">
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 1</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 2</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 3</h3>
+            </div>
+            <div className="work-1">
+              <img
+                src="https://placehold.co/600x400.png"
+                alt="works"
+                className="work-pic"
+              />
+              <h3>Work 4</h3>
+            </div>
+          </div>
+        </div>
+        <div className="contact"></div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Home;
